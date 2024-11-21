3fdff8bcbd5f86f1a4210a189d92bdfd6619bd9e
Lion Kortlepel <development@kortlepel.com>
2024-11-21 13:45:29 +0100

    make it work with object file generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use hf_codegen::compiler::CompilerSettings;
 
 fn main() {
@@ -14,39 +16,10 @@ fn main() {
     let target = hf_codegen::target::Target::native();
     let mut compiler = hf_codegen::compiler::HfCompiler::new(target, CompilerSettings::default());
 
-    #[cfg(target_os = "windows")]
-    let mut bytecode = vec![0x49, 0x89, 0xc8];
-    #[cfg(target_os = "linux")]
-    let mut bytecode = vec![0x49, 0x89, 0xf8];
-
-    bytecode.extend(
-        compiler
-            .compile_to_bytecode(ir)
-            .expect("successful compilation"),
-    );
-
-    bytecode.extend(vec![0xC3]);
-
-    unsafe {
-        run_code(bytecode);
-    }
-}
-
-unsafe fn run_code(code: Vec<u8>) {
-    use mmap_rs::MmapOptions;
-    let mapping = MmapOptions::new(MmapOptions::page_size())
-        .unwrap()
-        .map()
+    let obj = compiler
+        .compile_to_object_file(ir, &filename)
         .unwrap();
-    let mut mmap_mut = mapping.make_mut().unwrap();
-    for i in 0..code.len() {
-        mmap_mut[i] = code[i];
-    }
-    let map = mmap_mut.make_exec().unwrap();
-    let func: unsafe extern "C" fn(*mut std::ffi::c_void) = std::mem::transmute(map.as_ptr());
-    let buffer = vec![0u8; 4096].into_boxed_slice();
-    let buffer_ptr = Box::into_raw(buffer) as *mut std::ffi::c_void;
-    func(buffer_ptr);
-    let _ = unsafe { Box::from_raw(buffer_ptr as *mut [u8; 4096]) }; // Reclaim the memory
-    println!("you lived!");
+    let raw = obj.write().unwrap();
+    let out_filename = Path::new(&filename).with_extension("o");
+    std::fs::write(out_filename, raw).unwrap();
 }
